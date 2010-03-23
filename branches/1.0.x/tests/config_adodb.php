cb8fa18158b22fcd8885b73a11d61ce1bc499277
602000 <602000@1a378a6a-7532-11de-bf39-f1df4430aa60>
2010-03-23 16:53:00 +0000

    CoreMVC 1.0.0 Branch

---
+++ branches/1.0.x/tests/config_adodb.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'connect_provider' => 'adodb',
+	'connect_dsn' => 'mysqlt',
+	'connect_server' => 'localhost',
+	'connect_username' => 'ODBC',
+	'connect_password' => '',
+	'connect_dbname' => 'test',
+);
+?>
---
+++ branches/1.0.x/tests/config_pdo.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'connect_provider' => 'pdo',
+	'connect_dsn' => 'mysql:host=localhost;dbname=test',
+	'connect_username' => 'ODBC',
+	'connect_password' => '',
+);
+?>
