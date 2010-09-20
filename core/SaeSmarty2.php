82f68e3a32c78e499c68e37b112eb8631446365f
602000 <602000@1a378a6a-7532-11de-bf39-f1df4430aa60>
2010-09-20 13:56:00 +0000

    CoreMVC 1.3.0 Tag

--- core/SaeSmarty2.php
+++ core/SaeSmarty2.php
@@ -23,13 +23,13 @@ if( $smarty === null ) {
 	$smarty->template_dir = getcwd ();
 
 	// 编译路径
-	if ($mc = @memcache_init()) {
-		$smarty->compile_dir = 'saemc://smarty2_compile';
-		file_put_contents ($smarty->compile_dir, '');
+	if ($memcache = @memcache_init()) {
+		require_once dirname(__FILE__) . '/SaeMemcacheWrapper2.php';
+		SaeMemcacheWrapper2::$memcache = $memcache;
+		$smarty->compile_dir = 'saemc2://smarty_data';
 	} else {
 		$smarty->compile_dir = SAE_TMP_PATH;
 	}
-
 
 	// 注册函数
 	if (! function_exists ('smarty_function_view')) {
@@ -51,20 +51,10 @@ if( $smarty === null ) {
 	// 缓存函数
 	if (! function_exists ('smarty_function_cache')) {
 		function smarty_function_cache($action, &$smarty_obj, &$cache_content, $tpl_file=null, $cache_id=null, $compile_id=null, $exp_time=null){
-			static $path;
-			static $mc;
-			if ($path === null) {
-				$path = 'saemc://smarty2_cache';
-				file_put_contents($path,'');
-			}
 			$CacheID = md5($tpl_file.$cache_id.$compile_id);
-			$file = $path . '/' . $CacheID;
 			switch ($action) {
 				case 'read':
-					if (! file_exists ($file)){
-						break;
-					}
-					$data = file_get_contents ($file);
+					$data = memcache_get(SaeMemcacheWrapper2::$memcache, $CacheID);
 					if ($data === false) {
 						$cache_content = '';
 						$return = false;
@@ -74,14 +64,14 @@ if( $smarty === null ) {
 					}
 					break;
 				case 'write':
-					file_put_contents ($file, $cache_content);
+					memcache_set(SaeMemcacheWrapper2::$memcache, $CacheID, $cache_content);
 					$return = true;
 					break;
 				case 'clear':
 					if(empty($cache_id) && empty($compile_id) && empty($tpl_file)) {
 						$return = false;
 					} else {
-						unlink ($file);
+						memcache_delete(SaeMemcacheWrapper2::$memcache, $CacheID);
 						$return = true;
 					}
 					break;
@@ -92,7 +82,7 @@ if( $smarty === null ) {
 			return $return;
 		}
 	}
-	if ($mc) {
+	if ($memcache) {
 		$smarty->cache_handler_func = 'smarty_function_cache';
 	} else {
 		$smarty->cache_dir = SAE_TMP_PATH;
